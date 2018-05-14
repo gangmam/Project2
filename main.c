0941552e8ad3c6212dd21553b189a047fb6c095a
DoongX2 <comich1994@naver.com>
2018-05-14 17:37:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 struct stat stat1, stat2;
 struct tm *time1, *time2;
+int mon1, mon2, d1, d2;
+int h1, h2, min1, min2;
 
 void filestat1(void);
 void filestat2(void);
@@ -47,8 +49,42 @@ int main(void)
  void blockcmp(void){
  }
 
- void datecmp(void){
+ void datecmp(void){	
+  int result = 0;
+	if(d1 < d2)
+		result = 1;
+	else if(d1 > d2)
+		result = 2;
+  
+	if(mon1 < mon2)
+		result = 1;
+	else if(mon1 > mon2)
+		result = 2;
+
+	if(!result)
+		printf("same time\n");
+	else if(result == 1)
+		printf("text1 is younger\n");
+	else
+		printf("text2 is younger\n");
  }
 
  void timecmp(void){
+  	int result = 0;
+	if(min1 < min2)
+		result = 1;
+	else if(min1 > min2)
+		result = 2;
+  
+	if(h1 < h2)
+		result = 1;
+	else if(h1 > h2)
+		result = 2;
+
+	if(!result)
+		printf("same date\n");
+	else if(result == 1)
+		printf("text2 is younger\n");
+	else
+		printf("text2 is younger\n");
  }
