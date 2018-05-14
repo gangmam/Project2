af04f38b8f31f75d5c866eba613756d32a12ff15
김동우 <comich1994@naver.com>
2018-05-14 18:11:55 +0900

    완성

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
 struct stat stat1, stat2;
 struct tm *time1, *time2;
 
-int month1;
-int hour1;
-int day1;
-int min1;
+int month1, month2;
+int hour1, hour2;
+int day1, day2;
+int min1, min2;
 
 void filestat1(void);
 void filestat2(void);
@@ -103,8 +103,42 @@ void filestat2(void)
 	 }
  }
 
- void datecmp(void){
+ void datecmp(void) {
+	 int result = 0;
+	 if (day1 < day2)
+		 result = 1;
+	 else if (day1 > day2)
+		 result = 2;
+
+	 if (month1 < month2)
+		 result = 1;
+	 else if (month1 > month2)
+		 result = 2;
+
+	 if (!result)
+		 printf("same time\n");
+	 else if (result == 1)
+		 printf("text1 is younger\n");
+	 else
+		 printf("text2 is younger\n");
  }
 
- void timecmp(void){
+ void timecmp(void) {
+	 int result = 0;
+	 if (min1 < min2)
+		 result = 1;
+	 else if (min1 > min2)
+		 result = 2;
+
+	 if (hour1 < hour2)
+		 result = 1;
+	 else if (hour1 > hour2)
+		 result = 2;
+
+	 if (!result)
+		 printf("same date\n");
+	 else if (result == 1)
+		 printf("text2 is younger\n");
+	 else
+		 printf("text2 is younger\n");
  }
