c1e91a51e6065cbf712bcac5e1d4441fba805af4
김동우 <comich1994@naver.com>
2018-05-14 18:02:56 +0900

    no message

--- main.c
+++ main.c
@@ -36,71 +36,25 @@ int main(void)
 
 void filestat1(void)
 {
-	char* File1 = "Text_File1";
-	if (stat(File1, &stat1) < 0)
-	{
-		printf("Not exist %s\n", File1);
-		exit(0);
-	}
 }
 
 void filestat2(void)
 {
-	char* File2 = "Text_File2";
-	if (stat(File2, &stat2) <0)
-	{
-		printf("Not exist %s\n", File2);
-		exit(0);
-	}
 }
 
 void filetime1(void)
 {
-	time1 = localtime(&stat1.st_mtime);
-	month1 = time1->tm_mon;
-	day1 = time1->tm_mday;
-	hour1 = time1->tm_hour;
-	min1 = time1->tm_min;
 }
 
 void filetime2(void)
 {
-	time2 = localtime(&stat2.st_mtime);
-	mon2 = time2->tm_mon;
-	d2 = time2->tm_mday;
-	h2 = time2->tm_hour;
-	min2 = time2->tm_min;
 }
 
 void sizecmp(void)
 {
-	int file_size1 = stat1.st_size;
-	int file_size2 = stat2.st_size;
-
-	if (file_size1 == file_size2)
-		printf("sizes are equal\n");
-	else
-	{
-		if (file_size1 > file_size2)
-			printf("text1 is bigger\n");
-		else
-			printf("text2 is bigger\n");
-	}
 }
 void blockcmp(void)
 {
-	int file_blk_size1 = stat1.st_blksize;
-	int file_blk_size2 = stat2.st_blksize;
-
-	if (file_blk_size1 == file_blk_size2)
-		printf("sizes are equal\n");
-	else
-	{
-		if (file_blk_size1 > file_blk_size2)
-			printf("text1 is bigger\n");
-		else
-			printf("text2 is bigger\n");
-	}
 }
 
 void datecmp(void) {
